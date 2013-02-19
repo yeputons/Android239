c760a7ee97e8c7f181405196cf6d12ced1a3b428
yeputons <egor.suvorov@gmail.com>
2013-02-19 19:22:40 +0400

    Stub of Demo130218.java was created

---
+++ src/Demo130218.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: e.suvorov
+ * Date: 19.02.13
+ * Time: 19:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class Demo130218 {
+}
